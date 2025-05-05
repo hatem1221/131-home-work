377cb70861380eb0c04bae777498beff63a5e35e
hatem1221 <xitoomy@gmail.com>
2025-05-05 21:06:50 +0300

    Add files via upload

---
+++ H.homework/script.js
@@ -0,0 +1,15 @@
+function welcomeUser() {
+    let name = "أحمد";
+    alert("مرحبا بك يا " + name);
+  }
+  
+  function logMessage() {
+    console.log("أهلاً بك في لوحة التحكم!");
+  }
+  
+  function showUserInfo() {
+    let name = "أحمد";
+    let age = 25;
+    console.log("الاسم: " + name + ", العمر: " + age);
+  }
+  
